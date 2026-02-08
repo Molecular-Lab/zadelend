a19d6d20a2b3599d4ab961afb588ac7daae73704
WTSHAI <watthechai@gmail.com>
2026-02-08 13:30:39 +0700

    init web

---
+++ web/src/app/layout.tsx
@@ -0,0 +1,47 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+import { cookieToInitialState } from "wagmi";
+import { headers } from "next/headers";
+import { walletConfig } from "../config/wallet";
+import { GlobalProviders } from "../providers/GlobalProvider";
+import { Navbar } from "@/components/common/Navbar/Navbar";
+
+const interSans = Inter({
+  variable: "--font-inter-sans",
+  subsets: ["latin"],
+});
+
+const interMono = Inter({
+  variable: "--font-inter-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Pomwha",
+  description: "Bridge your assets with zk technology",
+};
+
+export default async function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const initialState = cookieToInitialState(
+    walletConfig,
+    (await headers()).get("cookie")
+  );
+  return (
+    <html lang="en">
+      <body
+        className={`${interSans.variable} ${interMono.variable} backdrop-blur-sm bg-gradient-to-br from-link-backgroundlight to-blue-100`}
+      >
+        <GlobalProviders initialState={initialState}>
+          <Navbar />
+          {children}
+        </GlobalProviders>
+      </body>
+    </html>
+  );
+}
